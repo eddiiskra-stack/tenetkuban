3d8a50c256519715600bf90d0657739c1816044a
AGOOD GUY <Eddi.iskra@gmail.com>
2025-12-29 12:39:12 +0000

    Добавьте код счетчика в HTML-код всех страниц сайта. Код нужно разместит

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -45,6 +46,19 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased">
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106067819', 'ym');
+
+            ym(106067819, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/106067819" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
         {children}
         <Toaster />
       </body>
